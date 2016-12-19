6f90cad515f20245e99dfd6f8da7844fa9de2162
Daniel Kocjan <dkocjan123@gmail.com>
2016-12-19 20:14:37 +0100

    Refactor posts_index.js from functional to class based component and insert log where future data would be fetched right before component mounts into app (in componentWillMount() function) based on it's lifecycle

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return <div>List of posts</div>;
-};
+class PostsIndex extends Component {
+  componentWillMount() {
+    console.log('blablablab');
+  }
+
+  render() {
+    return (
+      <div>List of posts</div>
+    );
+  }
+}
+
+export default PostsIndex;
