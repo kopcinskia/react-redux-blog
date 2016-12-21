72586a168cdaf4a5cd1bc5591e6f0ccd5cf93b1f
Daniel Kocjan <dkocjan123@gmail.com>
2016-12-21 16:12:19 +0100

    Add form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import { createPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsNew extends Component {
 
@@ -11,29 +12,57 @@ class PostsNew extends Component {
       <form onSubmit={ handleSubmit(this.props.createPost) }>
         <h4>Create A New Post</h4>
 
-        <div className="form-group">
-          <label>Title</label>
+        <div className={ `form-group ${ title.touched && title.invalid ? 'has-danger' : '' } ${ title.touched && title.valid ? 'has-success' : '' }` }>
+          <label className="form-control-label">Title</label>
           <input type="text" className="form-control" { ...title } />
+          <div className="form-control-feedback text-help">
+            { title.touched ? title.error : '' }
+          </div>
         </div>
 
-        <div className="form-group">
-          <label>Categories</label>
+        <div className={ `form-group ${ categories.touched && categories.invalid ? 'has-danger' : '' } ${ categories.touched && categories.valid ? 'has-success' : '' }` }>
+          <label className="form-control-label">Categories</label>
           <input type="text" className="form-control" { ...categories } />
+          <div className="form-control-feedback text-help">
+            { categories.touched ? categories.error : '' }
+          </div>
         </div>
 
-        <div className="form-group">
-          <label>Content</label>
+        <div className={ `form-group ${ content.touched && content.invalid ? 'has-danger' : '' } ${ content.touched && content.valid ? 'has-success' : '' }` }>
+          <label className="form-control-label">Content</label>
           <textarea className="form-control" rows="12" { ...content } />
+          <div className="form-control-feedback text-help">
+            { content.touched ? content.error : '' }
+          </div>
         </div>
 
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-outline-primary">Submit</button>
+        <Link to="/" className="btn btn-outline-danger">Cancel</Link>
       </form>
     );
   }
+}
 
+function validate(values) {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = 'Enter title';
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if (!values.content) {
+    errors.content = 'Enter content';
+  }
+  
+  return errors;
 }
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
